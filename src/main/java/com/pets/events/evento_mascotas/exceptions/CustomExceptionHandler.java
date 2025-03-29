24df0eddf29595ddd031b2297d99ee98015e45fb
Textic <danielbienchido@gmail.com>
2025-03-29 14:40:48 -0300

    feat: Se modifica el manejo de excepciones para que sea mas visible

---
+++ src/main/java/com/pets/events/evento_mascotas/exceptions/CustomExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.pets.events.evento_mascotas.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", ex.getStatusCode().value());
+        body.put("error", ((HttpStatus) ex.getStatusCode()).getReasonPhrase());
+        body.put("message", ex.getReason());
+		body.put("timestamp", System.currentTimeMillis());
+
+        return new ResponseEntity<>(body, ex.getStatusCode());
+    }
+}
