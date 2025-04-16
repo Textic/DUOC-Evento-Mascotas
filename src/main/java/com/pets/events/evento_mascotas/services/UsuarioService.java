25254e9d4458d309a40864e74d5d281f24fce86b
Textic <danielbienchido@gmail.com>
2025-04-16 14:52:04 -0400

    refactor: remove unused imports in UsuarioService

--- src/main/java/com/pets/events/evento_mascotas/services/UsuarioService.java
+++ src/main/java/com/pets/events/evento_mascotas/services/UsuarioService.java
@@ -1,9 +1,7 @@
 package com.pets.events.evento_mascotas.services;
 
 import com.pets.events.evento_mascotas.models.Usuario;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
